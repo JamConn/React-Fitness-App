98b253af4fe6c843d4c44325e0af139071463665
JamConn <20085217@mail.wit.ie>
2024-02-12 11:10:26 +0000

    Fixing Submodule error

---
+++ fitnessapp/src/Pages/homePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import BarChart from "../Charts/BarChart";
+
+
+const HomePage = () => {
+
+  return (
+
+    <><h2>Test for home page</h2><div classname="app">
+      {/* <BarChart />*/}
+      </div></> 
+
+
+
+  );
+
+};
+export default HomePage;
