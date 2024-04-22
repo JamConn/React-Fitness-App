8fb49a8646d529fb5d886cfa2274106309f54eb1
JamConn <20085217>
2024-04-22 23:49:33 +0100

    Prepare for Heroku deployment

--- FitFirstTime/index.js
+++ FitFirstTime/index.js
@@ -12,11 +12,15 @@ import { OAuth2Client } from 'google-auth-library';
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../fitnessapp/build')));
+
 
 
 
@@ -424,6 +428,11 @@ app.get('/latest-profile-pic', async (req, res) => {
   }
 });
 
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Fitnessapp/build', 'index.html'));
+});
 
 
 
